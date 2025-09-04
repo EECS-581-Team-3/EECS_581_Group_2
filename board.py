e288e7a52c2c1011c3dc6aefb4dee35200e81e15
Jake Kice <jake@kice.net>
2025-09-04 12:05:02 -0500

    Removed old unused function

--- board.py
+++ board.py
@@ -47,25 +47,6 @@ class Board:
             self.array[row][col].tag = 1 # set tag to cleared
 
         return self.array[row][col].val # this returns the val of the selected cell
-    
-
-        
-    def getAdjacentIndices(self, matrix, row, col):
-        rows = len(matrix)
-        cols = len(matrix[0]) if matrix else 0
-        
-        directions = [
-            (-1, 0), (1, 0), (0, -1), (0, 1),     # N, S, W, E
-            (-1, -1), (-1, 1), (1, -1), (1, 1)    # NW, NE, SW, SE
-        ]  # up, down, left, right
-        adjacent = []
-
-        for dr, dc in directions:
-            r, c = row + dr, col + dc
-            if 0 <= r < rows and 0 <= c < cols:
-                adjacent.append((r, c))
-
-        return adjacent
 
     def update_adjacency(self, row, col):
         '''Takes in a row and column value, increments adjacency values in valid adjacent cells'''
@@ -81,23 +62,6 @@ class Board:
                     continue
                 else:
                     self.array[row + i][col + j].val += 1   #Otherwise, increment val in target cell
-
-    '''
-    #def update(self):
-        # next we have to add adjacency values to each cell
-        # iterate through the array and look for a clear spot (0)
-        print("updating")
-        rowCount = -1
-        for row in self.array:
-            rowCount = rowCount + 1
-            for col in range(len(row)):
-                # when a clear spot is found, check every spot around it and add up mines
-                
-                if row[col].val == 0:
-                    for val in self.getAdjacentIndices(self.array, rowCount, col):
-                        print(val)
-        # after, set cell.val to the amount of mines found
-    '''
         
     def show_contents(self):
         '''Reveal contents of board by setting all cell tags to 1'''
