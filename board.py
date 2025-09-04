cadac8ca7b877cf4eccd4772caecb4347406c756
Jake Kice <jake@kice.net>
2025-09-03 20:45:45 -0500

    Added adjacency update function, incorporated into populate function

--- board.py
+++ board.py
@@ -7,6 +7,7 @@ class Board:
         self.size = size
         self.array = [[Cell() for i in range(size)] for j in range(size)]
         self.alive = True # changes to false when user gets blown up with a bomb
+        self.BOMB_VALUE = 9 #Value to indicate cell is a bomb
 
 
     def printArray(self): # now with labeled edges
@@ -27,9 +28,10 @@ class Board:
             row = rng.randint(0, self.size - 1)
             col = rng.randint(0, self.size - 1)
 
-            if self.array[row][col].val == 0: # if the cell is empty, place a mine and increment realCount
+            if self.array[row][col].val != self.BOMB_VALUE: # if the cell is not a bomb, place a mine and increment realCount
                 realCount = realCount + 1
-                self.array[row][col].val = 9 # place a mine. will be -1 but it's 9 for now for readability
+                self.array[row][col].val = self.BOMB_VALUE # place a mine.
+                self.update_adjacency(row, col) #Update adjacency value of adjacent cells
                 #print(f"mine {realCount} placed at {row}, {col}") # debug print statement
             
             else:
@@ -48,7 +50,7 @@ class Board:
     
 
         
-    def getAdjacentIndices(matrix, row, col):
+    def getAdjacentIndices(self, matrix, row, col):
         rows = len(matrix)
         cols = len(matrix[0]) if matrix else 0
         
@@ -65,7 +67,22 @@ class Board:
 
         return adjacent
 
+    def update_adjacency(self, row, col):
+        '''Takes in a row and column value, increments adjacency values in valid adjacent cells'''
+        for i in range(-1,2):   #Offsets for adjacent cells in row direction
+            if row + i < 0 or row + i >= len(self.array):   #If offset puts the target row off either side of the board, skip this offset
+                continue
+            for j in range(-1,2):   #Offsets for adjacent cells in column direction
+                if col + j < 0 or col + j >= len(self.array[0]):    #If offset puts the target column off either side of the board, skip this offset
+                    continue
+                elif i == 0 and j == 0: #If the target cell is the originating cell (offset 0,0), skip this cell
+                    continue
+                elif self.array[row + i][col + j].val == self.BOMB_VALUE:   #If target cell is already a bomb, skip this cell
+                    continue
+                else:
+                    self.array[row + i][col + j].val += 1   #Otherwise, increment val in target cell
 
+    '''
     #def update(self):
         # next we have to add adjacency values to each cell
         # iterate through the array and look for a clear spot (0)
@@ -79,19 +96,25 @@ class Board:
                 if row[col].val == 0:
                     for val in self.getAdjacentIndices(self.array, rowCount, col):
                         print(val)
-        
-        
         # after, set cell.val to the amount of mines found
+    '''
+        
+    def show_contents(self):
+        '''Reveal contents of board by setting all cell tags to 1'''
+        for i in range(len(self.array)):
+            for j in range(len(self.array[0])):
+                self.array[i][j].tag = 1
 
 
 
 
 
 
-
-#debug
-#b = Board(10)
-#b.populate(25)
-#b.select(0,1)
-#b.printArray()
+if __name__ == '__main__':
+    #debug
+    b = Board(10)
+    b.populate(25)
+    #b.select(0,1)
+    b.show_contents()
+    b.printArray()
 
